3a3d29ea1f680ada0d2a8790d5c4d5dd4aecb78b
Opilane TTHK <opilane@tthk.ee>
2023-04-03 14:32:24 +0300

    Edit page "Info" and "History". Update design

---
+++ history.php
@@ -0,0 +1,68 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Ajalugu Tapa Katleri</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<ul>
+    <h1 id="tkt">Tapa Katleri Taekwondo</h1>
+    <li><a href="main.php">Avaleht</a></li>
+    <li><a href="information.php">Info</a></li>
+    <li><a href="history.php">Ajalugu</a></li>
+    <li><a href="informationstudent.php">Õpilasele</a></li>
+    <li><a href="yearplan.php">Aastane plaan</a></li>
+</ul>
+
+<h1>Ajalugu</h1>
+<br><br>
+<div>
+<p>Taekwondo on Korea võitluskunst, mille asutas 11.aprillil 1955 Lõuna-Korea armee kindral Choi Hong Hee.<br> Lühikese eksisteerimise aja jooksul on Taekwondo saavutanud tohutu populaarsuse ja tänapäeval on harrastajate arv miljonites.</p>
+    <img src="img/generalchoihonghi.jpg" alt="General Choi Hong-hi">
+</div>
+<br>
+<div>
+    <h4>Tõlgitud Korea keelest:</h4>
+    <p>Tae-jala löök;<br> Kwon-sõrmenukk või löök käega;<br>Do-tee</p>
+    <h4>See tähendab, et taekwondos on kaks komponenti:</h4>
+    <p>Taekwon-käte ja jalgade kasutamine enesekaitseks,</p>
+    <p>Elutee, mis hõlmab intensiivset vaimset treeningut kõrgema teadvuse arendamiseks Taekwondo kultuuri ja filosoofia mõistmise kaudu, mis on idamaise filosoofilise tarkuse kvintessents.</p>
+</div>
+</body>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<!--footer-->
+<footer class="footer">
+    <div class="container bottom_border">
+        <div class="row">
+            <div class=" col-sm-4 col-md col-sm-4  col-12 col">
+                <h4 class="headin5_amrc col_white_amrc pt2">Spordiklubi Tapa Katleri</h4>
+                <!--headin5_amrc-->
+                <p class="mb10">Tervislik, kvaliteetne ja motiveeritud eluviis läbi spordi!</p>
+                <p class="mb10">Tule tegema taekwondo!</p>
+            </div>
+            <div class=" col-sm-4 col-md  col-6 col">
+                <h4 class="headin5_amrc col_white_amrc pt2">Tapa</h4>
+                <div class="textwidget"><div class="icon-block">
+                        <p><i class="fa fa-envelope"></i>&nbsp; tapa.katleri@gmail.com</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-mobile"></i>&nbsp; +372 5624 6990</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-building"></i>&nbsp; Tapa Vene Gümnaasium, Nooruse 2</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-briefcase"></i>&nbsp; Spordiklubi Tapa Katleri</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</footer>
+</html>
---
+++ main.php
@@ -0,0 +1,54 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Tapa Katleri Taekwondo</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<ul>
+    <h1 id="tkt">Tapa Katleri Taekwondo</h1>
+<li><a href="main.php">Avaleht</a></li>
+<li><a href="information.php">Info</a></li>
+<li><a href="history.php">Ajalugu</a></li>
+<li><a href="informationstudent.php">Õpilasele</a></li>
+<li><a href="yearplan.php">Aastane plaan</a></li>
+</ul>
+<h1>text</h1>
+</body>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<!--footer-->
+<footer class="footer">
+    <div class="container bottom_border">
+        <div class="row">
+            <div class=" col-sm-4 col-md col-sm-4  col-12 col">
+                <h4 class="headin5_amrc col_white_amrc pt2">Spordiklubi Tapa Katleri</h4>
+                <!--headin5_amrc-->
+                <p class="mb10">Tervislik, kvaliteetne ja motiveeritud eluviis läbi spordi!</p>
+                <p class="mb10">Tule tegema taekwondo!</p>
+            </div>
+            <div class=" col-sm-4 col-md  col-6 col">
+                <h4 class="headin5_amrc col_white_amrc pt2">Tapa</h4>
+                <div class="textwidget"><div class="icon-block">
+                        <p><i class="fa fa-envelope"></i>&nbsp; tapa.katleri@gmail.com</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-mobile"></i>&nbsp; +372 5624 6990</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-building"></i>&nbsp; Tapa Vene Gümnaasium, Nooruse 2</p>
+                    </div>
+                    <div class="icon-block">
+                        <p><i class="fa fa-briefcase"></i>&nbsp; Spordiklubi Tapa Katleri</p>
+                    </div>
+            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
